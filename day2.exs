a4f4f9798ba656cde61abf1edeee6e2790fdf98d
Kevin Tindall <kevinkjt2000@gmail.com>
2018-12-02 00:23:13 -0600

    Day 1 and 2 complete

---
+++ day2.exs
@@ -0,0 +1,288 @@
+data = """
+kqzxdenujwcsthbzgvyioflcsp
+vqwxdenujwcsthbmggyioflarp
+kqzxienujwcsthbmglyioclarp
+kuzxdetujwcsthbmgvyioflcrp
+kqnxdenujwcsthbmgvlooflarp
+kqzxdknpjwcsthwmgvyioflarp
+kgzxdenujwcsthbfgvyicflarp
+kqzxdenujrnsthbmgjyioflarp
+lqzxdeeujwcsthbmrvyioflarp
+iqfxdenujwcsthbmgvyiofyarp
+kvzxbenujwcstabmgvyioflarp
+kmzxdenujwcsthbmglyioolarp
+kqzxdenujhcsthbmgbyioflanp
+nqzxdenujwcsehbmgvsioflarp
+kqzlgenujwcsthbmgvyiofjarp
+kqzxdyfujwcsihbmgvyioflarp
+kqzxdsnujwcqthbmgvyiorlarp
+kqzxdenuywcsthbmgvyinflmrp
+knzxderujwcsthbmgvyioflaop
+kqxxdenujwczthbmgvyioflajp
+kqzxdevujwcsthbmgvyqoxlarp
+kqzxdenujwclmhbmgvyioslarp
+kqzldenujwcsthbmgvnisflarp
+kjtxdenujwcsthbmgvyfoflarp
+kqzxwenujwcstxbmgvyihflarp
+kqzxdenuhecsthbmgvyeoflarp
+kqzxdenhjwesthbmgvyioklarp
+kqkxdenujwcsthbcgvyiofgarp
+kqyxmenujwcsthbmgvyioflara
+kqzxdqnrjwcwthbmgvyioflarp
+kqzxdenufwcgyhbmgvyioflarp
+lqzxdenujwcsthbmtvyiofearp
+kqzxdenujwcsthbvgvthoflarp
+kqzxeenujwcsahbmgvyioflamp
+pqzxdenujwcsshbmjvyioflarp
+kqzxdesujwcstdbmgvyioflatp
+kqzxpenujwcsthimgvyioflhrp
+kqzxdmkujwcsthbmgvpioflarp
+kszxdenujwcsthbybvyioflarp
+kqzxdvnujwcsthbmgvyqoslarp
+kkzxdetujwcsthbmgvyiofltrp
+kqzxdenujwcsthomgvyiozlaro
+cqzfdenubwcsthbmgvyioflarp
+kqzxdenyjwcsthbmhvyiofldrp
+kqzxdenujwcsthbmghfiofxarp
+kmqxdebujwcsthbmgvyioflarp
+kqzxdenufwcsthbmvvypoflarp
+kqnxdenujwcsthbmgvtzoflarp
+bqzxdenujwcithbmgvyiohlarp
+kqzxdenurwrsthbmgvyioelarp
+kqzxdenujwcsthbmgpyiodlarl
+kqzxdengjwcxthbmgvjioflarp
+kizxdenujwcsnhqmgvyioflarp
+jqzxdenajwcsthbmnvyioflarp
+kqzcdenujwcsphbigvyioflarp
+kezxdenujwcsthbfgvyioflaqp
+kqzxdenujwcstybmgvyivfyarp
+kqzxdenujwcsthbmgvbiofsnrp
+kqzxdenujwcsthbmgvyhxfnarp
+kvzxdenqjfcsthbmgvyioflarp
+kqzxywnljwcsthbmgvyioflarp
+kqzxdenujwcsbhbzgvyioxlarp
+kqkxdenufwcsthbmgvyiofxarp
+dqzxddnujwcsthsmgvyioflarp
+yqrxdenujwcsthbagvyioflarp
+kqzxdenujwcsajbmgvyiovlarp
+kqztdunujwcsthbmgvyioilarp
+kqzxdequjwcsthbmgvyyoflarm
+kqzxdlnujwksthbmgvkioflarp
+tqvxdenujwcsthbmgveioflarp
+kqzndezupwcsthbmgvyioflarp
+kqzctsnujwcsthbmgvyioflarp
+kqzxdenujwmstkbmgvyioflgrp
+kqzxdenujwzsthdmgvyiofdarp
+kqzxdynujwcsthcmgvyioflasp
+kqzxdesujwcstybmgcyioflarp
+kqzxdenujwcsthbvgvyiyglarp
+kqzxpenujwcsthbogvyioflard
+khzxdenujwcsthbmgvyikflaep
+kqzxdedujwchthbmgvyeoflarp
+kxzxsepujwcsthbmgvyioflarp
+xqzxdenujwcsthbpgvyioelarp
+jfzxdenujwcsthbmgvyiollarp
+kqzxcenujwcethbmgvwioflarp
+kqzxdenujwcithbmgvysoflarg
+kqlxdenujwnsthbmgvyiotlarp
+wqzydenujwcsthbmgvyioftarp
+kqzxienuwwcsthbmgayioflarp
+kqzxdetujwcsthbmgvyhoflawp
+kqzxdqnujwrsthbmgvyxoflarp
+kqzxdenujwcvthbmgjiioflarp
+kqzxdenujwcjthbxgvaioflarp
+kqzxpenujwcsthymgvyioklarp
+kqzxdenujwcsthbmswygoflarp
+kqzxdenujwcsthbmgvyiaxiarp
+kqzxdenudkcsthbmgvyzoflarp
+kqzxdvndjwcsthbmgvyioflaxp
+kqzxdenujwcsthbmdvymoflvrp
+kqzxvenujwcsihbmgvyiofllrp
+kqzxdqnujwcsthbmgtyioflprp
+kqzxdenuuwcathbmgvsioflarp
+kqzrdenujwesthbjgvyioflarp
+kqzxdexujwcstzbmgvyyoflarp
+kqzxpenujwjstabmgvyioflarp
+kozxdenejwcsthbmgvpioflarp
+kbzxdenvjwcsthbmgvyiofsarp
+kolxdenujwcjthbmgvyioflarp
+kqzxdenujwcsthbmgvyiffsakp
+kqzxdelujwcsthbmlvyioflxrp
+kqzxdenugwcsthrmgvyioflprp
+kqzxdelujwcsthqmgvyiozlarp
+kqzxienujwosthbmgvykoflarp
+kqzxdeuujwicthbmgvyioflarp
+kqzxdenbjwcsthbmcvyaoflarp
+krzxdqnujwcsthbmgvyioflerp
+wqzxzenujwcsthbmgvyioclarp
+kqzxyenujwcsthbmgejioflarp
+kqzxdenujwcstsbmgvtidflarp
+kqnxdenejwcsthbmgvyioflara
+kqzsdmnujwcsthbmgvyioflaep
+kqzxdedujwnsthymgvyioflarp
+kqzxdenujwusthbmgnyioflarx
+kqzxlenujwcsthbmgayvoflarp
+kqzxdenujwcsthbmgvyiofngrh
+zqzxdenujwcsthbmgvyiofvxrp
+kqzydenujwmsthbmgvyiuflarp
+kqzxdenujkrsthbmdvyioflarp
+kqzxdlnujocsthbmgvyiofaarp
+kqzxdenujwcstybmgvyiofrwrd
+kqzxdenupwksthbmgvyiofbarp
+khzxdentjwcsthbmbvyioflarp
+kqzxdenujwcuphbmgvyihflarp
+kqzxdenhjwcgthbmgvyioflrrp
+kqzxdenujwcsthbmgvyiofakhp
+kqzxdenujwcstfkmgvyioflamp
+kqzxdenujqcsthbmgvkiorlarp
+kqzxdenujwcstvbmgvyioilasp
+kqzxdxnujwcsthbpgayioflarp
+kqzxdenupwysthbmgvyiofljrp
+kqzxdenujwcdthbmgvymoflarv
+kqnxdenujwcstvbmgvyixflarp
+kqjxdenujwcsthbmgvyikflurp
+kqsxdenulwcsthxmgvyioflarp
+bqzxbenujwcsahbmgvyioflarp
+vqzxdenujwcsthbmgvjzoflarp
+kqzhfenujwcsthimgvyioflarp
+eqzxdenujwcshhbmgnyioflarp
+kqzxdenujucstubmgvyicflarp
+kuzxdenuewcsthbmgvyiofuarp
+kqzxdenulwcsthbmgpyigflarp
+kqzxdebujwcsthbmgoyioflaro
+kqzxdenujwcuthbmgucioflarp
+kqzxdenujwcschpmgvyioflhrp
+kqzxfenujwcsthbmjvrioflarp
+kqzxdenujqcsthbmgvyndflarp
+kqzxdgnbjwcsthbmgvywoflarp
+kqzxdenujwcsthrmgtbioflarp
+yqzxdenyjwcsthbmgvyioflarg
+kqzxdenuxwxsthbmsvyioflarp
+kqzxdenujwcsthbugqyvoflarp
+qqzxdenujwcsahbmgoyioflarp
+kqsxdenudwcsthbmguyioflarp
+kqzxdenujwcstublgvyioflamp
+kqzxdemujwtstqbmgvyioflarp
+kqzxqvnajwcsthbmgvyioflarp
+kqzxoennjwcstbbmgvyioflarp
+kqzxfenujwcsthbmlvyioflwrp
+kqzjdunujwcsthhmgvyioflarp
+kqzxdenujwcqthbmgvyirfxarp
+kqzxdengjwcsthbmgvyiowlgrp
+kqgxdenujwcswhbmglyioflarp
+mqzxdekuuwcsthbmgvyioflarp
+kqzxdenujwdsthbmgbyiovlarp
+krzxdenlhwcsthbmgvyioflarp
+kqzxdenmjwcstqbmgvyioflanp
+kqzxdenujwcmthbmgvtioflyrp
+kqzxdenujwcsthbmgvaijflprp
+kqzxdenuywysqhbmgvyioflarp
+kqzxdenujwfsthbmgvyhoflark
+nqzcdefujwcsthbmgvyioflarp
+kqzxdenujrcsthgmgyyioflarp
+kqzxdqnujwzsthbmgvyioftarp
+kqzxdenujwcsthimgvyioolapp
+kqzxdenupwcsthbmggyioflyrp
+kqzxdjnujwcsthbvgvyioflarf
+kqzxdtnujwasthbmgvyiofuarp
+kqzxbensjzcsthbmgvyioflarp
+kqzxdenujwcsphbmwiyioflarp
+kqzgdenuowcsthbmgvyioflarh
+kmzxdenujwasthbmgvtioflarp
+kqzxdenujwcstybmgvyiofrard
+vqzxdenejwcsthbmglyioflarp
+kqhxdenujwcsmhbmgvyioflprp
+kqzxdnnujwcsthzsgvyioflarp
+kczxdenujwcsthbmgvyeoflaop
+kqzxdenujwcsxhbmgvaioflaap
+kqzxdenujwcsthbmgayiofnprp
+kqzxdvnujwcsthbmgvyipjlarp
+kqzxdenubwcskhbmgvyiofkarp
+kqzxdenujwcsthbgggyigflarp
+kqzxdenujncstabvgvyioflarp
+kqzxdenujwcstqimqvyioflarp
+kqzxeenujwcsdhbmgvyqoflarp
+kcpxdenujwcsthbmgvyioilarp
+kqwxuenujwcsthbmgvyiyflarp
+kqzxdwnujwcstgbmgvyioplarp
+kqzxdenuswcstvbmglyioflarp
+kqzxdenujwcsthabgvyiwflarp
+kqzxdpnujwcsthbmwvyiomlarp
+kqzxdenujwcdthbmgvcioffarp
+kqzxdenajwcsthbmtvyiofldrp
+kqzbnenujwcshhbmgvyioflarp
+kqzbdequiwcsthbmgvyioflarp
+kqzxdenuswcsohbmgzyioflarp
+kvzxdenujwcstdbmjvyioflarp
+kqzxoenujwcqthbmpvyioflarp
+kqzxhenujwcsthbmgoyiofoarp
+klzxdenujwczthbmgvyioflanp
+kqpxdenujwcsthbmgvyioflafz
+kqkxdenujwcstxbngvyioflarp
+kqzepenuxwcsthbmgvyioflarp
+bqzxdenujmcithbmgvyioflarp
+kdzxdjnujwcstnbmgvyioflarp
+kszxdenujwcsthbmgeyiofrarp
+kqzxdenijwcsthbmgvhiaflarp
+kqzadenujwcbtxbmgvyioflarp
+kqkxwenujwcsthbmgvyiowlarp
+pqzddenujwcsthbmgvyboflarp
+kqzxxenujwcsthbwgvyioflmrp
+kqzxdjnujwcsthbmgvyipilarp
+pqzxdenujwcsthbmgvyieflark
+sqzxdenujtcsthbmgiyioflarp
+kqzxdznujwcsthbmgvzioflajp
+kqzxdrnujqcsthbmgvyiofvarp
+gqzxdenujwcsthemgvlioflarp
+kqzxdenujjcsthbmgvuiofljrp
+kqzsdenujmcsthbmggyioflarp
+kqzxienujwcsthbmgvaioflaip
+kqzxdwnujwcstfkmgvyioflarp
+kqzqdenujwcithbmzvyioflarp
+kqzxdedpjwcsthbmgvyiofbarp
+kqzxdeaujwcbtdbmgvyioflarp
+kqzewenyjwcsthbmgvyioflarp
+kqzxddnujwcsthbmgyyiofrarp
+kqzxdtnujwcsthbmgvyiodlard
+kqzxdefujwcsthbmgvyiffwarp
+xczxdenujwcsthbmgvyooflarp
+kuzxdenujucsthbmgvykoflarp
+kqzxtenujwcwthbmgvyioplarp
+kqzxdencllcsthbmgvyioflarp
+"""
+
+ids = data |> String.split("\n", trim: true)
+
+_part1 =
+  ids
+  |> Enum.map(fn id ->
+    id
+    |> String.graphemes()
+    |> Enum.reduce(%{}, fn char, acc ->
+      Map.put(acc, char, (acc[char] || 0) + 1)
+    end)
+  end)
+  |> Enum.reduce({0, 0}, fn elem, _acc = {twos, threes} ->
+    {
+      twos + if(Enum.any?(Map.values(elem), fn v -> v == 2 end), do: 1, else: 0),
+      threes + if(Enum.any?(Map.values(elem), fn v -> v == 3 end), do: 1, else: 0)
+    }
+  end)
+  |> (fn {twos, threes} -> twos * threes end).()
+  |> IO.inspect()
+
+defmodule RC do
+  def comb(_, 0), do: [[]]
+  def comb([], _), do: []
+
+  def comb([h | t], m) do
+    for(l <- comb(t, m - 1), do: [h | l]) ++ comb(t, m)
+  end
+end
+
+_part2 =
+  ids
+  |> RC.comb(2)
+  |> Enum.max_by(fn [a, b] -> String.jaro_distance(a, b) end)
+  |> (fn [a, b] -> [a, b] end).()
+  |> IO.inspect()
