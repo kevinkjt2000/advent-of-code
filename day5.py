4291809c850c81a13446ddf86ff5aa6b211aa56c
Kevin Tindall <kevinkjt2000@gmail.com>
2018-12-05 00:09:40 -0600

    Improve day5 regex

--- day5.py
+++ day5.py
@@ -4,8 +4,8 @@ lDdhGghHHLAXxaKkFfDdsSKkeyYEwWDdlBbLIisGcCvOVQqvoVgGPAWwapgIiDJeiIOoElLBBbbGgrRY
 
 from copy import deepcopy
 import re
+regex = re.compile("(?P<letter>[a-zA-Z])(?!(?P=letter))(?i:(?P=letter))")
 
-regex = re.compile("aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz")
 
 for letter in "abcdefghijklmnopqrstuvwxyz":
     data = deepcopy(polymer)
