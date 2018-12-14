8e3e13197558d80aeee80a5820307462b3c80e51
Kevin Tindall <kevinkjt2000@gmail.com>
2018-12-13 23:34:23 -0600

    Inefficient part 2 complete?

--- day14.py
+++ day14.py
@@ -3,12 +3,18 @@ last = 894501
 state = [3, 7]
 recipes = 2
 elves = list(range(0, 2))
-while len(state) < last + 10:
+while True:
     state_sum = [int(digit) for digit in
                  str(sum([state[elf] for elf in elves]))]
     for num in state_sum:
         state.append(num)
     for (e, elf) in enumerate(elves):
         elves[e] = (1 + elf + state[elf]) % len(state)
+    try:
+        number = "".join([str(digit) for digit in state]).index(str(last))
+        print(number)
+        break
+    except ValueError:
+        pass
 
-print(elves, state[-10:])
+# print(elves, state[-10:])
