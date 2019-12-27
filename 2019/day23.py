6d243940d9f53e34ff6af99a924fff3327e219d0
Kevin Tindall <kevinkjt2000@gmail.com>
2019-12-26 21:48:19 -0600

    Day 23 part2 complete

--- 2019/day23.py
+++ 2019/day23.py
@@ -4,23 +4,10 @@ from time import sleep
 from intcode import run_program
 
 
+SLEEP_INTERVAL = 0.001
+NAT_X, NAT_Y = None, None
+NAT_LOCK = threading.Lock()
 DIE = threading.Event()
-
-
-# Thanks StackOverflow https://stackoverflow.com/a/325528/2607949
-class StoppableThread(threading.Thread):
-    """Thread class with a stop() method. The thread itself has to check
-    regularly for the stopped() condition."""
-
-    def __init__(self,  *args, **kwargs):
-        super(StoppableThread, self).__init__(*args, **kwargs)
-        self._stop_event = threading.Event()
-
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()
 
 
 if __name__ == "__main__":
@@ -28,12 +15,11 @@ if __name__ == "__main__":
     queues = [Queue() for i in range(50)]
     def create_receiver(net_addr):
         def receive():
-            print("booting", net_addr)
             yield net_addr
             while not DIE.is_set():
                 if queues[net_addr].empty():
                     yield -1
-                    sleep(0.01)
+                    sleep(SLEEP_INTERVAL)
                 else:
                     x, y = queues[net_addr].get()
                     yield x
@@ -42,24 +28,38 @@ if __name__ == "__main__":
 
     threads = []
     for net_addr in range(50):
-        def work():
+        def work(net_addr):
+            global NAT_X, NAT_Y
             og = run_program(program.copy(), create_receiver(net_addr))
-            while True:
+            while not DIE.is_set():
                 try:
                     dest_net_addr, x, y = next(og), next(og), next(og)
                     if dest_net_addr == 255:
-                        print("answer", y)
-                        DIE.set()
-                        break
-                    print("sending", (dest_net_addr, x, y))
+                        with NAT_LOCK:
+                            NAT_X, NAT_Y = x, y
+                        continue
                     queues[dest_net_addr].put((x, y))
                 except StopIteration:
                     break
-        t = StoppableThread(target=work)
+        t = threading.Thread(target=work, args=(net_addr,))
         threads.append(t)
         t.start()
 
-    for t in threads:
-        if DIE.is_set():
-            t.stop()
-        t.join(0.01)
+    def nat_work():
+        prev_y = None
+        while not DIE.is_set():
+            with NAT_LOCK:
+                if NAT_Y and all(map(lambda q: q.empty(), queues)):
+                    if prev_y == NAT_Y:
+                        DIE.set()
+                        print("duplicate NAT_Y", NAT_Y)
+                        break
+                    queues[0].put((NAT_X, NAT_Y))
+                    prev_y = NAT_Y
+            sleep(SLEEP_INTERVAL)
+    nat_thread = threading.Thread(target=nat_work)
+    nat_thread.start()
+
+    while any(map(lambda t: t.is_alive(), threads)):
+        t.join(SLEEP_INTERVAL)
+    nat_thread.join()
