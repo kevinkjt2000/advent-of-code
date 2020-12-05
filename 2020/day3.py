083c310811ed190a8dc7addf1c23c6e177a3be70
Kevin Tindall <kevinkjt2000@gmail.com>
2020-12-04 20:21:15 -0600

    Part 2 complete

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,7 +1,12 @@
-import re
+from functools import reduce
+from operator import mul
 
 
-def part1():
+def product(collection):
+    return reduce(mul, collection, 1)
+
+
+def part1(dx, dy):
     tree_lines = []
     with open("./day3.input") as file_input:
         for line in file_input.readlines():
@@ -10,22 +15,27 @@ def part1():
     total_collisions = 0
     n = len(tree_lines)
     x, y = (0, 0)
-    dx, dy = (3, 1)
     while y < n:
         x %= len(tree_lines[0])
         if tree_lines[y][x] == "#":
             total_collisions += 1
         x += dx
         y += dy
-    print(total_collisions)
+    return total_collisions
 
 
 def part2():
-    pass
+    return product([
+        part1(1, 1),
+        part1(3, 1),
+        part1(5, 1),
+        part1(7, 1),
+        part1(1, 2),
+    ])
 
 
 def main():
-    part1()
+    print(part2())
 
 
 if __name__ == "__main__":
