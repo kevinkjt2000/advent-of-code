e52f9ccd11e0b79b0f27d6a248961f541b23895a
Kevin Tindall <kevinkjt2000@gmail.com>
2020-12-12 22:31:12 -0600

    Complete day12 part2

--- 2020/day12.py
+++ 2020/day12.py
@@ -27,4 +27,32 @@ def part1():
     return abs(dx) + abs(dy)
 
 
-print(part1())
+def part2():
+    instructions = list(read_input())
+    dx, dy = 0, 0
+    wx, wy = 10, 1
+    for letter, number in instructions:
+        if letter == "F":
+            dx += wx * number
+            dy += wy * number
+        elif letter == "N":
+            wy += number
+        elif letter == "S":
+            wy -= number
+        elif letter == "E":
+            wx += number
+        elif letter == "W":
+            wx -= number
+        elif (letter, number) in [("L", 90), ("R", 270)]:
+            wx, wy = -wy, wx
+        elif (letter, number) in [("L", 180), ("R", 180)]:
+            wx, wy = -wx, -wy
+        elif (letter, number) in [("L", 270), ("R", 90)]:
+            wx, wy = wy, -wx
+    return abs(dx) + abs(dy)
+
+
+# 2458 too low
+
+
+print(part2())
