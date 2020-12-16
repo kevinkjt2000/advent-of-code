8f3d6e7a7b098a539eb1fcf8a321c274d7054749
Kevin Tindall <kevinkjt2000@gmail.com>
2020-12-15 21:33:18 -0600

    Committing my lengthy version that took hours to compute the answer for day13 part2

--- 2020/day13.py
+++ 2020/day13.py
@@ -1,3 +1,7 @@
+from itertools import count, filterfalse, islice
+from tqdm import tqdm
+
+
 def find_smallest_multiple_of_k_greater_than_or_equal_to_n(n, k):
     rem = (n + k) % k
     if (rem == 0):
@@ -21,4 +25,30 @@ def part1():
     return best_bus * (best_timestamp - earliest_timestamp)
 
 
-print(part1())
+def part2():
+    with open("./day13.input") as file_input:
+        lines = file_input.readlines()
+        buses = [(int(b), i) for (i, b) in enumerate(lines[1].split(",")) if b != "x"]
+    pbar = tqdm()
+
+    buses = sorted(buses, key=lambda btup: btup[0], reverse=True)
+    print(buses)
+    print(list(islice(filterfalse(lambda t: (t + buses[1][1]) % buses[1][0], count(buses[0][1], buses[0][0])), 0, 2)))
+    return
+
+    dt = 17429 # buses[0][0]
+    t = 4147 # buses[-1][0]
+    while True:
+        pbar.update()
+        cool = True
+        for (b, i) in buses:
+            if 0 != (t + i) % b:
+                cool = False
+                break
+        if cool:
+            pbar.close()
+            return t
+        t += dt
+
+
+print(part2())
