93967014129c95a5162b66c714a07023702b4a1a
Kevin Tindall <kevinkjt2000@gmail.com>
2020-12-15 22:22:07 -0600

    Finished a part2 for day13 that I'm not ashamed of

--- 2020/day13.py
+++ 2020/day13.py
@@ -1,13 +1,14 @@
+from functools import reduce
 from itertools import count, filterfalse, islice
 from tqdm import tqdm
 
 
 def find_smallest_multiple_of_k_greater_than_or_equal_to_n(n, k):
     rem = (n + k) % k
-    if (rem == 0):
+    if rem == 0:
         return n
     else:
-        return (n + k - rem)
+        return n + k - rem
 
 
 def part1():
@@ -16,39 +17,42 @@ def part1():
         earliest_timestamp = int(lines[0])
         buses = [int(b) for b in lines[1].split(",") if b != "x"]
     best_bus = buses[0]
-    best_timestamp = find_smallest_multiple_of_k_greater_than_or_equal_to_n(n=earliest_timestamp, k=best_bus)
+    best_timestamp = find_smallest_multiple_of_k_greater_than_or_equal_to_n(
+        n=earliest_timestamp, k=best_bus
+    )
     for b in buses:
-        timestamp = find_smallest_multiple_of_k_greater_than_or_equal_to_n(n=earliest_timestamp, k=b)
+        timestamp = find_smallest_multiple_of_k_greater_than_or_equal_to_n(
+            n=earliest_timestamp, k=b
+        )
         if timestamp < best_timestamp:
             best_timestamp = timestamp
             best_bus = b
     return best_bus * (best_timestamp - earliest_timestamp)
 
 
+def find_next_overlap(bus1, bus2):
+    t, dt = bus1
+    i, b = bus2
+    magic_numbers = list(
+        islice(
+            filterfalse(
+                lambda _t: (_t + i) % b,
+                count(t, dt),
+            ),
+            0,
+            2,
+        )
+    )
+    return (magic_numbers[0], magic_numbers[1] - magic_numbers[0])
+
+
 def part2():
     with open("./day13.input") as file_input:
         lines = file_input.readlines()
-        buses = [(int(b), i) for (i, b) in enumerate(lines[1].split(",")) if b != "x"]
-    pbar = tqdm()
+        buses = [(i, int(b)) for (i, b) in enumerate(lines[1].split(",")) if b != "x"]
 
-    buses = sorted(buses, key=lambda btup: btup[0], reverse=True)
-    print(buses)
-    print(list(islice(filterfalse(lambda t: (t + buses[1][1]) % buses[1][0], count(buses[0][1], buses[0][0])), 0, 2)))
-    return
-
-    dt = 17429 # buses[0][0]
-    t = 4147 # buses[-1][0]
-    while True:
-        pbar.update()
-        cool = True
-        for (b, i) in buses:
-            if 0 != (t + i) % b:
-                cool = False
-                break
-        if cool:
-            pbar.close()
-            return t
-        t += dt
+    t, _ = reduce(find_next_overlap, buses[1:], buses[0])
+    return t
 
 
 print(part2())
